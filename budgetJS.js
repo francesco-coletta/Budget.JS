e431745ea5d621970d62220119ac3d18010065fe
francesco-coletta <francesco.coletta@tiscali.it>
2015-01-03 01:50:48 +0100

    Added list of simple posts

--- budgetJS.js
+++ budgetJS.js
@@ -10,5 +10,16 @@ angular.module('budgetJS', [])
     ['$scope', 
      function($scope){
             $scope.test = "Hello Budget JS";  
+
+            $scope.posts = [
+                "post 1",
+                "post 2",
+                "post 3",
+                "post 4",
+                "post 5",
+                "post 6",
+                "post 7"
+            ];
         }
     ]);
+
