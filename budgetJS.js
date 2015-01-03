2af90e0b6a485cd4efce5d184af0fe8c39e18362
francesco-coletta <francesco.coletta@tiscali.it>
2015-01-03 16:59:43 +0100

    Al post aggiunto il nuovo attributo Link

--- budgetJS.js
+++ budgetJS.js
@@ -15,19 +15,20 @@ angular.module('budgetJS', [])
             $scope.posts = [
                 {title: "post 1", upvotes: 5 },
                 {title: "post 2", upvotes: 0 },
-                {title: "post 3", upvotes: 6 },
-                {title: "post 4", upvotes: 5 },
-                {title: "post 5", upvotes: 2 },
-                {title: "post 6", upvotes: 1 },
-                {title: "post 7", upvotes: 0 }
+                {title: "post 3", upvotes: 6 }
             ];
 
             $scope.addPost = function()
             {
                 if ($scope.title && !($scope.title === ""))
                 {
-                    $scope.posts.push({title: $scope.title, upvotes: 0 });
+                    $scope.posts.push({
+                        title: $scope.title,
+                        link: $scope.link,
+                        upvotes: 0 
+                    });
                     $scope.title = "";
+                    $scope.link = "";
                 };
             };
 
