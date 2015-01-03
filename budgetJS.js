1db9a8ccfd97420af2f1c81ad281a2ff2462b33a
francesco-coletta <francesco.coletta@tiscali.it>
2015-01-03 01:54:11 +0100

    Added other info to posts list

--- budgetJS.js
+++ budgetJS.js
@@ -12,13 +12,13 @@ angular.module('budgetJS', [])
             $scope.test = "Hello Budget JS";  
 
             $scope.posts = [
-                "post 1",
-                "post 2",
-                "post 3",
-                "post 4",
-                "post 5",
-                "post 6",
-                "post 7"
+                {title: "post 1", upvotes: 5 },
+                {title: "post 2", upvotes: 0 },
+                {title: "post 3", upvotes: 6 },
+                {title: "post 4", upvotes: 5 },
+                {title: "post 5", upvotes: 2 },
+                {title: "post 6", upvotes: 1 },
+                {title: "post 7", upvotes: 0 }
             ];
         }
     ]);
