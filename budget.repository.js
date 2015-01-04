a885a2679e955c647615ae1012fd1d3db7f9df2a
francesco-coletta <francesco.coletta@tiscali.it>
2015-01-04 01:13:37 +0100

    Refactory

---
+++ budget.repository.js
@@ -0,0 +1,15 @@
+/**
+*  Post Repository
+*/
+angular.module('budget.repository', [])
+.factory('postRepository', function(){
+        var posts = [
+                {title: "post 1", upvotes: 5 },
+                {title: "post 2", upvotes: 0 },
+                {title: "post 3", upvotes: 6 }
+            ];
+
+        return {
+            posts: posts
+        };
+    });
